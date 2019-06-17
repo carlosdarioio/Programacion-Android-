7bce542e42dc677b26e872f90fd2bd41dbf53fa0
Carlos Dario Flores <cdfn3@hotmail.com>
2019-06-17 17:00:41 -0600

    Cambios 20190617
    Buscando como programar un tab en android studio

---
+++ MyAppTabTest2/app/src/main/java/com/example/myapptabtest2/ui/main/PlaceholderFragment.java
@@ -0,0 +1,73 @@
+package com.example.myapptabtest2.ui.main;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.myapptabtest2.R;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class PlaceholderFragment extends Fragment {
+
+    private static final String ARG_SECTION_NUMBER = "section_number";
+    public static int numero=0;
+
+    private PageViewModel pageViewModel;
+
+    public static PlaceholderFragment newInstance(int index) {
+        PlaceholderFragment fragment = new PlaceholderFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt(ARG_SECTION_NUMBER, index);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        pageViewModel = ViewModelProviders.of(this).get(PageViewModel.class);
+        int index = 1;
+        if (getArguments() != null) {
+            index = getArguments().getInt(ARG_SECTION_NUMBER);
+        }
+        //obtendiendo
+        numero=index; //num de ARG_SECTION_NUMBER seleccionadp
+
+        pageViewModel.setIndex(index);
+    }
+
+    @Override
+    public View onCreateView(
+            @NonNull final LayoutInflater inflater, final ViewGroup container,
+            Bundle savedInstanceState) {
+
+        //asignando fragment
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+
+
+        final TextView textView = root.findViewById(R.id.section_label);
+
+
+
+
+
+        pageViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+
+            }
+        });
+        return root;
+    }
+}
