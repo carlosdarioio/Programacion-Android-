86c210524fdc1e75e2adfdb6790754099b7a549d
Carlos Dario Flores <cdfn3@hotmail.com>
2019-06-25 15:38:43 -0600

    Cambios 20190625
    Se agreglo al json la repsueta de verificacion dle ingreso, el cual si es correcto el resultado tendria un color diferente por respuesta.
    
    al apretar enter ejecuta la misma accion que apretar el boton

--- CnfrmFact/app/src/main/java/com/example/cnfrmfact/Main2ActivityFacturaScan.kt
+++ CnfrmFact/app/src/main/java/com/example/cnfrmfact/Main2ActivityFacturaScan.kt
@@ -2,9 +2,12 @@ package com.example.cnfrmfact
 
 import android.content.Intent
 import android.content.SharedPreferences
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.KeyEvent
+import android.view.View
 import android.widget.Toast
 import com.android.volley.Request
 import com.android.volley.Response
@@ -14,6 +17,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main2_factura_scan.*
 import org.json.JSONException
 import org.json.JSONObject
+import java.util.concurrent.TimeUnit
 
 class Main2ActivityFacturaScan : AppCompatActivity() {
     var mApp = GlobalsVar()
@@ -29,12 +33,32 @@ class Main2ActivityFacturaScan : AppCompatActivity() {
         user=sharedPref.getString(mApp.PREF_USER,null)
         Toast.makeText(this@Main2ActivityFacturaScan, "Bienvenido "+user, Toast.LENGTH_SHORT).show()
 
+
+        editnumero.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
+                //Perform Code
+                JSONScanFacture()
+                return@OnKeyListener true
+            }
+            false
+        })
+
+
+        //------
         btnEnviar.setOnClickListener {
 
+            if(editnumero.text.toString()!="")
+            {
             JSONScanFacture()
-
-
+            }
+            else
+            {
+                Toast.makeText(this@Main2ActivityFacturaScan, "rrellenar datos ", Toast.LENGTH_SHORT).show()
+            }
         }
+
+        //-------
+
 
     }//fin increate
 
@@ -50,16 +74,27 @@ class Main2ActivityFacturaScan : AppCompatActivity() {
                 Log.d("strrrrr", ">>$response")
                 try {
 
-
                     val xresponse = JSONObject(response)
-
                     //arrays
                     //val mJSONArray = xresponse.getJSONArray("contacts")//nombre de objec json
                     //val mJSONObject = mJSONArray.getJSONObject(2)
 
                     val RespestaJson = xresponse.getString("R")
+                    val status = xresponse.getString("status")
+
+                    if(status=="1")//ingresada
+                    {
+                        txtResult.setTextColor(Color.parseColor("#4444DD"))
+                    }
+                    else
+                    {
+                        txtResult.setTextColor(Color.parseColor("#BB4444"))
+                    }
+                    
                     Toast.makeText(this@Main2ActivityFacturaScan, "Response:$RespestaJson", Toast.LENGTH_LONG).show()
+                   
                     txtResult.text=RespestaJson
+                    //limpiar txtResult en 5 seconds
 
                 } catch (e: JSONException) {
                     Toast.makeText(this@Main2ActivityFacturaScan, "Errorx ", Toast.LENGTH_LONG).show()
@@ -75,5 +110,13 @@ class Main2ActivityFacturaScan : AppCompatActivity() {
         val requestQueue = Volley.newRequestQueue(this)
         requestQueue.add(stringRequest)
 
+
+        editnumero.text.clear()
     }///Fin JsonObject---------------
+
+
+    //Borrar label en 5 segundos
+
+
+    //Fin borrar label en 5 segundos
 }
