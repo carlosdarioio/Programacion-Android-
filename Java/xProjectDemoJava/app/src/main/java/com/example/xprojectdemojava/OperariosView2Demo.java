17da7b6f21a7a13e1cbb9b5a388b1a1124c31438
Carlos Dario Flores <cdfn3@hotmail.com>
2019-12-12 22:44:51 -0600

    Cambios 20191212 v2
    Formato Select (datos locales).
    Formato Select (on changedatos locales).
    Terminados

--- Java/xProjectDemoJava/app/src/main/java/com/example/xprojectdemojava/OperariosView2Demo.java
+++ Java/xProjectDemoJava/app/src/main/java/com/example/xprojectdemojava/OperariosView2Demo.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class OperariosView2Demo extends AppCompatActivity {
 //clase solo para estableces formulairo que se usaran (select, listas
@@ -50,14 +54,62 @@ public class OperariosView2Demo extends AppCompatActivity {
         Formato label SubTema.
         Formato label Descripcion.
         Formaro Boton.
-        Formato Select (datos locales)
+        Formato Select (datos locales).
+        Formato Select (on changedatos locales).
         Formato Select (datos Json)
-        Formato descirpcion
+        Formato Select (on changedatos Json)
         Formato gridview
         * */
 
+        //spinner----------------------------
+        //Agregando valores y listandolos
+        String[] arraySpinner = new String[] {"025", "002", "053", "045" };
+        Spinner s = (Spinner) findViewById(R.id.spinner_Select);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, arraySpinner);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s.setAdapter(adapter);
+
+        //Accion si seleciona x spinner
+        s.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                //----------
+                //spinner2----------------------------Si selecciona el 3 actualiza
+                if(position==3){
+                //Actualizando valores y listandolos
+                String[] arraySpinner3 = new String[] { "C025", "C002", "C053", "C045"        };
+                Spinner sC = (Spinner) findViewById(R.id.spinner_Select2);
+                ArrayAdapter<String> adapter3 = new ArrayAdapter<String>(OperariosView2Demo.this,
+                        android.R.layout.simple_spinner_item, arraySpinner3);
+                adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                sC.setAdapter(adapter3);
+                 }
+                //fin spinnder3-----------------------
+
+                //-------------
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+
+        });
+        //fin spinnder-----------------------
+
+        //spinner2----------------------------
+        //Asignando valores
+        String[] arraySpinner2 = new String[] {"z025", "z002", "z053", "z045"};
+        Spinner sz = (Spinner) findViewById(R.id.spinner_Select2);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, arraySpinner2);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sz.setAdapter(adapter2);
+        //fin spinnder2-----------------------
 
     }//Fin Oncreate
+
 
     //--------------------------------MENUUUUUUUU
     @Override
@@ -87,6 +139,5 @@ public class OperariosView2Demo extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
     //------------------
-
 
 }//Fin
